2c72ec53749d18b26a7294cf04bfd8f94a3edf41
Xavier Lopez <ecks910@yahoo.com>
2026-01-26 13:37:42 -0800

    wip

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,2 +1,11 @@
 module EntriesHelper
+  def safe_entry_url(entry)
+    raw = entry&.url.to_s
+    uri = URI.parse(raw)
+    return raw if uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
+
+    nil
+  rescue URI::InvalidURIError
+    nil
+  end
 end
